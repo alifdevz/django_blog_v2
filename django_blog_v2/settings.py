f23b54c2cf65c87417d3d5c9113383789a0a79c5
alifdevz <alifm.devz@gmail.com>
2021-09-15 16:29:27 +0800

    Fix key issue

--- django_blog_v2/settings.py
+++ django_blog_v2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=fln&o=cu5#g1(un)6k&s6(sjrn)pvj1c=tnn&6-16fxc6bq(x'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
